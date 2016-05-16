921cf789d3e2e6cc772d072e83a6daa571eaed51
Brent Johnson <brent@brents-mac-mini.local>
2016-05-15 21:27:13 -0400

    Initial Commit

---
+++ Cannon WarTests/Cannon_WarTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cannon_WarTests.swift
+//  Cannon WarTests
+//
+//  Created by Brent Johnson on 5/15/16.
+//  Copyright © 2016 Brent Johnson. All rights reserved.
+//
+
+import XCTest
+@testable import Cannon_War
+
+class Cannon_WarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
